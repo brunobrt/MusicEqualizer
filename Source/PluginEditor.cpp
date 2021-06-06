2bfebcef531b06d34cd962365a52513bbd97728e
Bruno Bartolomasi <brunobrt@hotmail.com>
2021-06-05 21:30:11 -0300

    chore(app): first commit

---
+++ Source/PluginEditor.cpp
@@ -0,0 +1,40 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+MusicEqualizerAudioProcessorEditor::MusicEqualizerAudioProcessorEditor (MusicEqualizerAudioProcessor& p)
+    : AudioProcessorEditor (&p), audioProcessor (p)
+{
+    // Make sure that before the constructor has finished, you've set the
+    // editor's size to whatever you need it to be.
+    setSize (400, 300);
+}
+
+MusicEqualizerAudioProcessorEditor::~MusicEqualizerAudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void MusicEqualizerAudioProcessorEditor::paint (juce::Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+
+    g.setColour (juce::Colours::white);
+    g.setFont (15.0f);
+    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+}
+
+void MusicEqualizerAudioProcessorEditor::resized()
+{
+    // This is generally where you'll want to lay out the positions of any
+    // subcomponents in your editor..
+}
